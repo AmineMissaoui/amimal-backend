d9a8648a8ba5f0f8c399da2a0694790a5712db87
AmineMissaoui <amine.missaoui@esprit.tn>
2021-06-16 20:07:19 +0100

    api platforme

---
+++ migrations/Version20210614180451.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210614180451 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE refuge ADD longitude DOUBLE PRECISION NOT NULL, ADD latitude DOUBLE PRECISION NOT NULL, ADD place_disponible INT NOT NULL, ADD telephone VARCHAR(255) NOT NULL, ADD description LONGTEXT NOT NULL, ADD contact_principale VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE refuge DROP longitude, DROP latitude, DROP place_disponible, DROP telephone, DROP description, DROP contact_principale');
+    }
+}
